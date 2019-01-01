6b7921cdcf4c844ca5293964cfd3fd240d93617f
Nikhil Reddy Araga <nikhil.araga@hitachivantara.com>
2019-01-01 11:43:20 +0530

    Created jenkins installation script for centos7

---
+++ install_jenkins/jenkins_centos7.sh
@@ -0,0 +1,19 @@
+### Installing stable jenkins on RHEL-7 and centos-7
+## install wget vim tree
+### Ref https://www.vultr.com/docs/how-to-install-jenkins-on-centos-7
+
+
+sudo yum install wget vim tree java-1.8.0-openjdk.x86_64 -y
+
+sudo cp /etc/profile /etc/profile_backup
+echo 'export JAVA_HOME=/usr/lib/jvm/jre-1.8.0-openjdk' | sudo tee -a /etc/profile
+echo 'export JRE_HOME=/usr/lib/jvm/jre' | sudo tee -a /etc/profile
+source /etc/profile
+
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+yum install jenkins -y
+
+sudo systemctl start jenkins.service
+sudo systemctl enable jenkins.service
+

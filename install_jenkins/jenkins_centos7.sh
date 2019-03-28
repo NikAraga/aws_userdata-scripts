028d8a676f905a18dd831f835a36d7d211a3d916
Nikhil Araga <nikhilreddy.araga@gmail.com>
2019-03-28 09:09:41 +0530

    Updated sudo in jenkins installation

--- install_jenkins/jenkins_centos7.sh
+++ install_jenkins/jenkins_centos7.sh
@@ -14,8 +14,7 @@ source /etc/profile
 
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-yum install jenkins -y
+sudo yum install jenkins -y
 
 sudo systemctl start jenkins.service
 sudo systemctl enable jenkins.service
-

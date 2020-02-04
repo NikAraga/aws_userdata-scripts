0b661a9d44bf6a048359d069002b6e245b20f9c2
Nikhil Araga <nikhilreddy.araga@gmail.com>
2020-02-04 12:22:00 +0530

    Update java-jdk_8.sh

--- java-jdk_8/java-jdk_8.sh
+++ java-jdk_8/java-jdk_8.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo yum install wget -y
 
-## Install java-jdk_8
+## Install java-jdk_8 on centos7 machine
 wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.rpm"
 sudo yum localinstall jdk-8u131-linux-x64.rpm -y
